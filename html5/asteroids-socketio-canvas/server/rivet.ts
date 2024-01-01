02f04023cd41f94ac9617054035a25b94c7789f5
ABCxFF <79597906+ABCxFF@users.noreply.github.com>
2023-12-31 20:26:28 -0500

    Bring in fundamentals
    Co-Authored-By: Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>

---
+++ html5/asteroids-socketio-canvas/server/rivet.ts
@@ -0,0 +1,23 @@
+import { RivetClient } from "@rivet-gg/api";
+
+export const RIVET = new RivetClient({ token: process.env.RIVET_TOKEN });
+
+export async function ready() {
+    try {
+        // Notify Rivet that this lobby is ready to accept players
+        return await RIVET.matchmaker.lobbies.ready();
+    } catch (e) {
+        console.log(":(", e);
+        process.exit(1);
+    }
+}
+
+export async function playerConnected(playerToken: string) {
+    // Tell Rivet that the player has connected.
+    return await RIVET.matchmaker.players.connected({ playerToken });
+}
+
+export async function playerDisconnected(playerToken: string) {
+    // Tell Rivet that the player has connected.
+    return await RIVET.matchmaker.players.disconnected({ playerToken });
+}
